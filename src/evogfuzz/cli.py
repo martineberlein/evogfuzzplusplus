b5a3995209c88a5cae191fef45b86d976b7b0350
martineberlein <schuldner-01.vordach@icloud.com>
2023-01-07 19:18:10 +0100

    restructure project

--- src/evogfuzz/cli.py
+++ src/evogfuzz/cli.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello World")
+    print("CLI not supported yet! Coming soon!")
 
 
 if __name__ == '__main__':
---
+++ tests/test_evogfuzz.py
@@ -0,0 +1,23 @@
+import unittest
+from evogfuzz.evogfuzz_class import EvoGFuzz
+
+from test_data import GRAMMAR, INITIAL_INPUTS, prop, fitness_function
+
+
+class TestEvoGFuzz(unittest.TestCase):
+
+    def test_evogfuzz_initialize(self):
+        evogfuzz = EvoGFuzz(
+            grammar=GRAMMAR,
+            inputs=INITIAL_INPUTS,
+            prop=prop,
+            fitness_function=fitness_function,
+        )
+        evogfuzz.initialize()
+
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
