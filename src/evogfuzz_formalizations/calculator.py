6da9894ac702e1d14d4a0382e4066360afa3245e
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-02-28 18:45:25 +0100

    better calculator

--- src/evogfuzz_formalizations/calculator.py
+++ src/evogfuzz_formalizations/calculator.py
@@ -1,40 +1,59 @@
-import string
 import math
+import logging
+
 from fuzzingbook.Grammars import Grammar
-from fuzzingbook.Parser import DerivationTree
+
+from evogfuzz.oracle import OracleResult
+from evogfuzz.input import Input
 
 
-GRAMMAR: Grammar = {
+def arith_eval(inp: Input) -> float:
+    return eval(
+        str(inp), {"sqrt": math.sqrt, "sin": math.sin, "cos": math.cos, "tan": math.tan}
+    )
+
+
+def prop_(inp: Input) -> bool:
+    try:
+        arith_eval(inp)
+        return False
+    except ValueError:
+        return True
+
+
+def prop(inp: Input) -> OracleResult:
+    try:
+        return OracleResult.BUG if prop_(inp) else OracleResult.NO_BUG
+    except SyntaxError:
+        logging.info(
+            f"Input {str(inp)} is not a valid input of the program. You might want to rewrite your grammar!"
+        )
+        return OracleResult.UNDEF
+
+
+grammar: Grammar = {
+    "<start>": ["<function>(<term>)"],
+    "<function>": ["sqrt", "tan", "cos", "sin"],
+    "<term>": ["-<value>", "<value>"],
+    "<value>": ["<integer>.<integer>", "<integer>"],
+    "<integer>": ["<digit><integer>", "<digit>"],
+    "<digit>": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+}
+
+
+grammar_alhazen: Grammar = {
     "<start>": ["<arith_expr>"],
     "<arith_expr>": ["<function>(<number>)"],
     "<function>": ["sqrt", "sin", "cos", "tan"],
     "<number>": ["<maybe_minus><onenine><maybe_digits><maybe_frac>"],
     "<maybe_minus>": ["", "-"],
     "<onenine>": [str(num) for num in range(1, 10)],
-    "<digit>": list(string.digits),
+    "<digit>": [str(num) for num in range(0, 10)],
     "<maybe_digits>": ["", "<digits>"],
     "<digits>": ["<digit>", "<digit><digits>"],
     "<maybe_frac>": ["", ".<digits>"],
 }
 
+START_SYMBOL = "<start>"
 
-INITIAL_INPUTS = ["cos(10)", "sqrt(28367)", "tan(-12)", "sqrt(3)"]
-
-
-def _arith_eval(inp: DerivationTree) -> float:
-    return eval(
-        str(inp), {"sqrt": math.sqrt, "sin": math.sin, "cos": math.cos, "tan": math.tan}
-    )
-
-
-def prop(inp: DerivationTree) -> bool:
-    """
-    Evaluation function, that returns 'True' when the calculator fails.
-    :param inp:
-    :return:
-    """
-    try:
-        _arith_eval(inp)
-        return False
-    except ValueError:
-        return True
+initial_inputs = ["cos(12)", "sqrt(-900)"]
