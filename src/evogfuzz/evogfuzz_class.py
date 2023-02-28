1c60d31044497088daa7606023607d83ae3e1404
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-02-28 18:47:11 +0100

    restructure main class to handle new input format

--- src/evogfuzz/evogfuzz_class.py
+++ src/evogfuzz/evogfuzz_class.py
@@ -7,16 +7,19 @@ from enum import Enum
 from copy import deepcopy
 
 from fuzzingbook.Grammars import Grammar
-from fuzzingbook.Parser import EarleyParser, DerivationTree
+from fuzzingbook.Parser import EarleyParser
 from fuzzingbook.ProbabilisticGrammarFuzzer import (
     is_valid_probabilistic_grammar,
     ProbabilisticGrammarMiner,
     ProbabilisticGrammarFuzzer,
 )
+from isla.derivation_tree import DerivationTree
 
 from evogfuzz.tournament_selection import Tournament
 from evogfuzz.fitness_functions import fitness_function_failure
 from evogfuzz import helper
+from evogfuzz.oracle import OracleResult
+from evogfuzz.input import Input
 
 
 class GrammarType(Enum):
@@ -31,17 +34,16 @@ class EvoGFuzz:
     def __init__(
         self,
         grammar: Grammar,
-        prop: Callable[[Union[DerivationTree, str]], bool],
+        prop: Callable[[Union[Input, str]], OracleResult],
         inputs: List[str],
         fitness_function: Callable[
-            [Set[Tuple[DerivationTree, bool]]], Set[Tuple[DerivationTree, bool, float]]
+            [Input], float
         ] = fitness_function_failure,
         iterations: int = 10,
         working_dir: Path = None,
     ):
         self.grammar = grammar
-        self._prop = prop
-        self.initial_inputs = set(inputs)
+        self._prop: Callable[[Input], OracleResult] = prop
         self.working_dir = working_dir
         self._probabilistic_grammars: List[Tuple[Grammar, GrammarType, float]] = []
         self._iteration: int = 0
@@ -51,88 +53,86 @@ class EvoGFuzz:
         self._elitism_rate: int = 5
         self._tournament_size: int = 10
         self._tournament_number: int = 10
-        self._all_data = None
+        self._all_inputs = None
         self._avg_prev_data = 0
         self.fitness_function: Union[
             Callable[
-                [Set[Tuple[DerivationTree, bool]]],
-                Set[Tuple[DerivationTree, bool, float]],
+                [Input],
+                float,
             ],
             None,
         ] = fitness_function
-        self._probabilistic_grammar_miner = None
+
+        self._probabilistic_grammar_miner = ProbabilisticGrammarMiner(
+            EarleyParser(self.grammar)
+        )
+
+        self.inputs = set()
+        for inp in inputs:
+            self.inputs.add(
+                Input(
+                    DerivationTree.from_parse_tree(
+                        next(EarleyParser(grammar).parse(inp))
+                    )
+                )
+            )
 
         # Apply patch to fuzzingbook
         helper.patch()
 
-    def execute(self):
+    def setup(self):
+        for inp in self.inputs:
+            inp.oracle = self._prop(inp)
+
+        probabilistic_grammar = self._learn_probabilistic_grammar(self.inputs)
+        self._probabilistic_grammars.append(
+            (deepcopy(probabilistic_grammar), GrammarType.LEARNED, -1)
+        )
+        initial_population = self._generate_input_files(probabilistic_grammar)
+
+        return initial_population
+
+    def fuzz(self):
         logging.info("Fuzzing with EvoGFuzz")
-        self.initialize()
+        new_population: Set[Input] = self.setup()
         while self._do_more_iterations():
             logging.info(f"Starting iteration {self._iteration}")
-            self._loop()
+            new_population = self._loop(new_population)
             self._iteration = self._iteration + 1
         self._finalize()
 
     def optimize(self) -> Grammar:
         logging.info("Optimizing with EvoGFuzz")
-        self.initialize()
         while self._do_more_iterations():
             logging.info("Starting to optimize probabilities")
 
         # Return best Grammar
         return Grammar
 
-    def _loop(self):
-        # generate input files
-        new_inputs = self._generate_input_files()
-        # execute input files
-        execution_outcome = self._execute_input_files(new_inputs)
-        data = set()
-        for i in zip(new_inputs, execution_outcome):
-            data.add(i)
+    def _loop(self, test_inputs: Set[Input]):
+        # obtain labels, execute samples (Initial Step, Activity 5)
+        for inp in test_inputs:
+            inp.oracle = self._prop(inp)
+
         # determine fitness of individuals
-        fitness = self._determine_fitness(data)
+        for inp in test_inputs:
+            inp.fitness = self.fitness_function(inp)
+
         # select fittest individuals
-        fittest_individuals = self._select_fittest_individuals(fitness)
+        fittest_individuals: Set[Input] = self._select_fittest_individuals(test_inputs)
+
         # learn new probabilistic grammar
-        self._learn_new_grammar(fittest_individuals)
-        # mutate grammar
-        self._mutate_grammar()  # TODO Only mutate when no improvement
-        # collect new data
-        self._add_new_data(fittest_individuals)
-        # display stats
-        # self.__show_stats()
-
-    def initialize(self):
-        execution_outcome = self._execute_input_files(self.initial_inputs)
-        logging.info("exec: " + str(execution_outcome))
-        logging.info("inp: " + str(self.initial_inputs))
-
-        data = zip(self.initial_inputs, execution_outcome)
-        logging.info("zip_list: " + str(list(data)))
-        # positive_data: Set[Tuple[str, bool]] = {inp for inp in data if inp[1] is True}
-        # positive_inputs: List[str] = [inp[0] for inp in positive_data]
-
-        logging.info("New Learning.")
-        self._probabilistic_grammar_miner = ProbabilisticGrammarMiner(
-            EarleyParser(self.grammar)
-        )
-        probabilistic_grammar = (
-            self._probabilistic_grammar_miner.mine_probabilistic_grammar(
-                list(self.initial_inputs)
-            )
-        )
-
-        logging.debug("Learned probabilistic grammar:")
-        for rule in probabilistic_grammar:
-            logging.debug(rule.ljust(40) + str(probabilistic_grammar[rule]))
-
+        probabilistic_grammar = self._learn_probabilistic_grammar(fittest_individuals)
         self._probabilistic_grammars.append(
             (deepcopy(probabilistic_grammar), GrammarType.LEARNED, -1)
         )
 
-        assert is_valid_probabilistic_grammar(probabilistic_grammar)
+        # mutate grammar
+        mutated_grammar = self._mutate_grammar(probabilistic_grammar)
+        self._probabilistic_grammars.append((mutated_grammar, GrammarType.MUTATED, -1))
+
+        # generate new population
+        return self._generate_input_files(mutated_grammar)
 
     def _do_more_iterations(self):
         if -1 == self._max_iterations:
@@ -142,77 +142,61 @@ class EvoGFuzz:
             return False
         return True
 
-    def _generate_input_files(self):
-        grammar = self._get_latest_grammar()
-        probabilistic_fuzzer = ProbabilisticGrammarFuzzer(grammar)
-        new_inputs = set()
+    def _generate_input_files(self, probabilistic_grammar):
+        logging.info("Generating new Test Inputs")
+        probabilistic_fuzzer = ProbabilisticGrammarFuzzer(probabilistic_grammar)
+        new_test_inputs = set()
         for _ in range(self._number_individuals):
-            new_inputs.add(probabilistic_fuzzer.fuzz())
+            new_test_inputs.add(Input(
+                DerivationTree.from_parse_tree(probabilistic_fuzzer.fuzz_tree())
+                )
+            )
+        return new_test_inputs
 
-        logging.info(f"Generated {len(new_inputs)} unique inputs")
-        return new_inputs
-
-    def _execute_input_files(
-        self, input_samples: Set[Union[DerivationTree, str]]
-    ) -> List[bool]:
-        logging.info("Executing input files")
-
-        exec_oracle = []
-        for inp in input_samples:
-            exec_oracle.append(self._prop(inp))
-
-        return exec_oracle
-
-    def _determine_fitness(self, data: Set[Tuple[DerivationTree, bool]]):
-        return self.fitness_function(data)
-
-    def _safe_fitness_for_grammar(self, sum_fitness):
-        g = self._probabilistic_grammars.pop()
-        lst = list(g)
-        lst[2] = sum_fitness
-        self._probabilistic_grammars.append(tuple(lst))
+    def _safe_fitness_for_grammar(self, sum_fitness: float):
+        grammar_tuple: Tuple[Grammar, GrammarType, float] = self._probabilistic_grammars.pop()
+        grammar, grammar_type, _ = grammar_tuple
+        self._probabilistic_grammars.append((grammar, grammar_type, sum_fitness))
 
     def _select_fittest_individuals(
-        self, data
-    ) -> Set[Tuple[DerivationTree, bool, float]]:
+        self, test_inputs: Set[Input]
+    ) -> Set[Input]:
+
         fittest_individuals = Tournament(
-            data, self._tournament_number
+            test_inputs, self._tournament_number
         ).select_fittest_individuals()
-        sum_fitness = sum([x[2] for x in fittest_individuals])
-        logging.info(
-            f"Current probabilistic grammar achived a fitness of: {sum_fitness}"
+
+        sum_fitness = sum([inp.fitness for inp in fittest_individuals])
+        logging.debug(
+            f"Current probabilistic grammar achieved a combined fitness of: {sum_fitness}"
         )
         self._safe_fitness_for_grammar(sum_fitness)
 
         return fittest_individuals
 
-    def _learn_new_grammar(self, fittest_individuals):
+    def _learn_probabilistic_grammar(self, test_inputs: Set[Input]):
         logging.info("Learning new Grammar")
 
-        # Unpacking input samples
-        input_samples = list(x[0] for x in fittest_individuals)
-        logging.info(input_samples)
+        input_strings = list(str(inp.tree) for inp in test_inputs)
 
-        probabilistic_grammar_miner = ProbabilisticGrammarMiner(
-            EarleyParser(self.grammar)
-        )
-        new_probabilistic_grammar = (
-            probabilistic_grammar_miner.mine_probabilistic_grammar(input_samples)
+        probabilistic_grammar = (
+            self._probabilistic_grammar_miner.mine_probabilistic_grammar(input_strings)
         )
         assert is_valid_probabilistic_grammar(
-            new_probabilistic_grammar
+            probabilistic_grammar
         ), "Exit! Newly generated Grammar is not valid!"
 
-        self._probabilistic_grammars.append(
-            (deepcopy(new_probabilistic_grammar), GrammarType.LEARNED, -1)
-        )
+        return probabilistic_grammar
 
-    def _mutate_grammar(self):
+    @staticmethod
+    def _mutate_grammar(probabilistic_grammar):
         logging.info("Mutating new Grammar")
 
-        mutated_grammar = deepcopy(self._get_latest_grammar())
+        mutated_grammar = deepcopy(probabilistic_grammar)
 
-        # if prev_avg < avg_fitness*1.025 -> Mutate
+        # TODO if prev_avg < avg_fitness*1.025 -> Mutate
+
+        # Only select production_rules with > 2 alternatives
         filtered = list(
             filter(lambda x: len(mutated_grammar[x]) > 1, list(mutated_grammar))
         )
@@ -227,13 +211,7 @@ class EvoGFuzz:
         for rule in mutated_grammar:
             logging.info(rule.ljust(30) + str(mutated_grammar[rule]))
 
-        self._probabilistic_grammars.append((mutated_grammar, GrammarType.MUTATED, -1))
-
-    def _add_new_data(self, fittest_individuals):
-        if self._all_data is None:
-            self._all_data = fittest_individuals
-        else:
-            self._all_data.update(fittest_individuals)
+        return mutated_grammar
 
     def _finalize(self):
         logging.info("Exiting EvoGFuzz!")
@@ -246,4 +224,3 @@ class EvoGFuzz:
 
     def _get_latest_grammar(self):
         return self._probabilistic_grammars[-1][0]
-
