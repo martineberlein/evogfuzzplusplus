3e688f1b643d63000aea977518e083cdf9d16079
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-07-03 14:41:54 +0200

    black

--- tests/test_evoggen.py
+++ tests/test_evoggen.py
@@ -11,7 +11,6 @@ from evogfuzz.evogfuzz_class import EvoGGen
 from evogfuzz.input import Input
 from evogfuzz.oracle import OracleResult
 
-
 def oracle(inp: Input | str):
     def program(inp_):
         if eval(str(inp_)) <= -8:
@@ -24,7 +23,6 @@ def oracle(inp: Input | str):
     except SyntaxError:
         return OracleResult.UNDEF
 
-
 grammar: Grammar = {
     "<start>": ["<number>"],
     "<number>": ["<maybe_minus><one_nine>"],
@@ -34,11 +32,10 @@ grammar: Grammar = {
 
 initial_inputs = ["-8", "1"]
 
-
 def oracle_2(inp: Input | str):
     def program(inp_: str):
-        elements = inp_.split(";")
-        assert not (-9 <= eval(str(elements[0])) <= -1 and elements[1] == "fuzzer")
+        elements = inp_.split(';')
+        assert not (-9 <= eval(str(elements[0])) <= -1 and elements[1] == 'fuzzer')
 
     try:
         program(str(inp))
@@ -48,20 +45,18 @@ def oracle_2(inp: Input | str):
     except SyntaxError:
         return OracleResult.UNDEF
 
-
 grammar_2: Grammar = {
     "<start>": ["<number>;<string>"],
     "<number>": ["<maybe_minus><one_nine><digits>"],
     "<maybe_minus>": ["-", ""],
     "<one_nine>": [str(i) for i in range(1, 10)],
     "<digits>": ["<digit><digits>", ""],
-    "<digit>": [str(i) for i in range(0, 10)],
-    "<string>": ["<letter><string>", "<letter>"],
-    "<letter>": [char for char in string.ascii_letters],
+    "<digit>": [str(i) for i in range(0,10)],
+    "<string>": ['<letter><string>', '<letter>'],
+    "<letter>": [char for char in string.ascii_letters]
 }
 
 initial_inputs_2 = ["-8;fuzzer", "6;se"]
-
 
 class TestEvoGGen(unittest.TestCase):
     def setUp(self) -> None:
@@ -75,6 +70,7 @@ class TestEvoGGen(unittest.TestCase):
 
         assert oracle(initial_inputs[0]) == OracleResult.BUG
         assert oracle(initial_inputs[1]) == OracleResult.NO_BUG
+
 
         # Case #2: oracle_2, grammar_2, and initial_inputs_2
         assert is_valid_grammar(grammar=grammar_2)
@@ -124,7 +120,7 @@ class TestEvoGGen(unittest.TestCase):
         for i in failing_inputs_clean:
             print(i)
         # self.assertEqual(generator_grammar, expected_grammar)
-        # self.assertEqual(failing_inputs_clean, {"-8", "-9"})
+        #self.assertEqual(failing_inputs_clean, {"-8", "-9"})
 
     def test_evoggen_transform_grammar(self):
         evo = EvoGGen(
@@ -132,17 +128,16 @@ class TestEvoGGen(unittest.TestCase):
             inputs=initial_inputs_2,
             oracle=oracle_2,
             iterations=100,
-            transform_grammar=True,
+            transform_grammar=True
         )
 
         generator_grammar, failing_inputs = evo.optimize()
         failing_inputs_clean = {str(inp) for inp in failing_inputs}
 
         expected_result = set()
-        for res in product([str(i) for i in range(-9, 0)], ["fuzzer"]):
+        for res in product([str(i) for i in range(-9, 0)], ['fuzzer']):
             expected_result.add(f"{res[0]};{res[1]}")
         self.assertSetEqual(expected_result, failing_inputs_clean)
-
 
 if __name__ == "__main__":
     unittest.main()
