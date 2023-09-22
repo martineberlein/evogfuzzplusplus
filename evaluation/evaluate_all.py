b181177b70fbd2170848a2e03654e519316c771f
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-09-22 09:52:56 +0200

    refined t4p pipeline

--- evaluation/evaluate_all.py
+++ evaluation/evaluate_all.py
@@ -3,12 +3,13 @@ from typing import List, Type
 from subject import EvaluationSubject
 from tools import Tool, GrammarBasedEvaluationFuzzer, InputsFromHellEvaluationFuzzer, EvoGFuzzEvaluationTool,ISLaGrammarEvaluationFuzzer
 from evaluate_calculator import CalculatorSubject
+from subject import Pysnooper2Tests4pyEvaluationSubject
 
 
 if __name__ == "__main__":
 
     subjects: List[Type[EvaluationSubject]] = [
-        CalculatorSubject,
+        Pysnooper2Tests4pyEvaluationSubject,
     ]
 
     tools: List[Type[Tool]] = [
@@ -18,15 +19,15 @@ if __name__ == "__main__":
         EvoGFuzzEvaluationTool
     ]
 
-    default_param = {"max_iterations": 100, "timeout": 3600, "log": True}
+    default_param = {"max_iterations": 60, "timeout": 3600, "log": True, "max_nonterminals": 5, "max_generated_inputs": 10000}
 
     results = dict()
     for subject in subjects:
-        param = (subject.
-                 build().
-                 get_evaluation_config(default_param)
-                 )
+        print(subject.__name__)
+        subject = subject()
+        param = subject.get_evaluation_config(default_param)
 
+        print(param)
         for tool in tools:
             tool = tool(**param)
             print(tool.run(), end="\n\n")
