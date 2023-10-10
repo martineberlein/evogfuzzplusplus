75815ed3290c33fa1b34adf06b7b42d1fcf8dd81
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-10-10 22:45:47 +0200

    added a subject factory for mpi projects

--- evaluation/mpi_helper.py
+++ evaluation/mpi_helper.py
@@ -1,68 +1,138 @@
 import importlib.util
 import sys
-from typing import Union
+from typing import Union, Type, List, Callable, Dict
 from pathlib import Path
-
-from fuzzingbook.Grammars import Grammar
+from abc import ABC
 import string
 
-from evogfuzz.input import Input
+from fuzzingbook.Grammars import Grammar
 from evogfuzz.oracle import OracleResult
 from evogfuzz.oracle_construction import construct_oracle
 
 
-grammar: Grammar = {
-    '<start>': ["<input>"],
-    "<input>": ["<first> <second>"],
-    "<first>": ["<integer>"],
-    "<second>": ["<integer>"],
-    "<integer>": ["<onenine><maybe_digits>"],
-    "<onenine>": [str(num) for num in range(1, 10)],
-    "<digit>": list(string.digits),
-    "<maybe_digits>": ["", "<digits>"],
-    "<digits>": ["<digit>", "<digit><digits>"],
-}
-
-
 class TestSubject:
     def __init__(self, grammar=None, oracle=None, test_inputs=None):
-        self.grammar = grammar or self.default_grammar()
+        self.grammar = grammar or self.default_grammar
         self.oracle = oracle or self.default_oracle()
-        self.test_inputs = test_inputs or self.default_test_inputs()
+        self.test_inputs = test_inputs or self.default_test_inputs
 
-    def default_grammar(self):
-        return {}
+    default_grammar = {}
+    default_test_inputs = []
 
     def default_oracle(self):
-        return None
-
-    def default_test_inputs(self):
-        return []
+        raise ValueError("Default Oracle not implemented")
 
     def to_dict(self):
         return {
-            'grammar': self.grammar,
-            'oracle': self.oracle,
-            'inputs': self.test_inputs,
+            "grammar": self.grammar,
+            "oracle": self.oracle,
+            "inputs": self.test_inputs,
         }
 
 
-class GCDTestSubject(TestSubject):
-    def default_grammar(self):
-        return grammar
+class MPITestSubject(TestSubject, ABC):
+    base_path: str
+    implementation_class_name: str = "Solution"
+    implementation_function_name: str
 
-    def default_oracle(self):
-        error_def = {TimeoutError: OracleResult.UNDEF}
-        return construct_oracle(ob.gcd, ref.gcd, error_def, default_oracle_result=OracleResult.BUG)
+    @classmethod
+    def ground_truth(cls) -> Callable:
+        solution_file_path = cls.base_path / Path("reference1.py")
+        return load_module_dynamically(
+            solution_file_path,
+            cls.implementation_class_name,
+            cls.implementation_function_name,
+        )
 
-    def default_test_inputs(self):
-        return ["10 2", "4 4"]
+
+class GCDTestSubject(MPITestSubject):
+    base_path = Path("./resources/mpi/problem_1_GCD")
+    implementation_function_name = "gcd"
+    default_grammar: Grammar = {
+        "<start>": ["<input>"],
+        "<input>": ["<first> <second>"],
+        "<first>": ["<integer>"],
+        "<second>": ["<integer>"],
+        "<integer>": ["<one_nine><maybe_digits>"],
+        "<one_nine>": [str(num) for num in range(1, 10)],
+        "<digit>": list(string.digits),
+        "<maybe_digits>": ["", "<digits>"],
+        "<digits>": ["<digit>", "<digit><digits>"],
+    }
+    default_test_inputs = ["10 2", "4 4"]
 
 
-def load_module_dynamically(path: Union[str, Path], class_name: str, function_name: str):
+class SquareRootTestSubject(MPITestSubject):
+    base_path = Path("./resources/mpi/problem_10_Square-root")
+    implementation_function_name = "floorSqrt"
+    default_grammar: Grammar = {
+        "<start>": ["<input>"],
+        "<input>": ["<integer>"],
+        "<integer>": ["<one_nine><maybe_digits>", "0"],
+        "<one_nine>": [str(num) for num in range(1, 10)],
+        "<digit>": list(string.digits),
+        "<maybe_digits>": ["", "<digits>"],
+        "<digits>": ["<digit>", "<digit><digits>"],
+    }
+    default_test_inputs = ["4", "5"]
+
+
+class MiddleTestSubject(MPITestSubject):
+    base_path = Path("./resources/mpi/problem_7_Middle-of-Three")
+    implementation_function_name = "middle"
+    default_grammar: Grammar = {
+        "<start>": ["<input>"],
+        "<input>": ["<first> <second> <third>"],
+        "<first>": ["<integer>"],
+        "<second>": ["<integer>"],
+        "<third>": ["<integer>"],
+        "<integer>": ["<one_nine><maybe_digits>"],
+        "<one_nine>": [str(num) for num in range(1, 10)],
+        "<digit>": list(string.digits),
+        "<maybe_digits>": ["", "<digits>"],
+        "<digits>": ["<digit>", "<digit><digits>"],
+    }
+    default_test_inputs = ["978 518 300", "162 934 200"]
+
+
+class MPITestSubjectFactory:
+    def __init__(self, test_subject_type: Type[MPITestSubject]):
+        self.test_subject_type = test_subject_type
+
+    def build(
+        self,
+        err_def: Dict[Exception, OracleResult] = None,
+        default_oracle: OracleResult = None,
+    ) -> List[MPITestSubject]:
+        subjects = []
+        for i in range(1, 11):
+            buggy_file_path = self.test_subject_type.base_path / Path(
+                f"prog_{i}/buggy.py"
+            )
+            loaded_function = load_module_dynamically(
+                buggy_file_path,
+                self.test_subject_type.implementation_class_name,
+                self.test_subject_type.implementation_function_name,
+            )
+            error_def = err_def or {TimeoutError: OracleResult.UNDEF}
+            def_oracle = default_oracle or OracleResult.BUG
+            oracle = construct_oracle(
+                loaded_function,
+                self.test_subject_type.ground_truth(),
+                error_def,
+                default_oracle_result=def_oracle,
+                timeout=0.01,
+            )
+            subjects.append(self.test_subject_type(oracle=oracle))
+        return subjects
+
+
+def load_module_dynamically(
+    path: Union[str, Path], class_name: str, function_name: str
+):
     # Step 1: Convert file path to module name
-    file_path = str(path)
-    module_name = file_path.replace('/', '.').rstrip('.py')
+    file_path = str(path.absolute())
+    module_name = file_path.replace("/", ".").rstrip(".py")
 
     # Step 2: Load module dynamically
     spec = importlib.util.spec_from_file_location(module_name, file_path)
@@ -74,12 +144,14 @@ def load_module_dynamically(path: Union[str, Path], class_name: str, function_na
     return function
 
 
-def get_gcd_subjects(err_def=None, default_oracle=None):
-    subjects = []
-    for i in range(1,11):
-        result = load_module_dynamically(f'/Users/martineberlein/github/EvoGFuzzplusplus/evaluation/resources/mpi/problem_1_GCD/prog_{i}/buggy.py', 'Solution', 'gcd')
-        error_def = err_def or {TimeoutError: OracleResult.UNDEF}
-        def_oracle = default_oracle or OracleResult.BUG
-        oracle = construct_oracle(result, ref.gcd, error_def, default_oracle_result=def_oracle, timeout=0.01)
-        subjects.append(GCDTestSubject(oracle=oracle))
-    return subjects
+def main():
+    factory = MPITestSubjectFactory(MiddleTestSubject)
+    subjects = factory.build()
+    for subject in subjects:
+        oracle = subject.to_dict().get("oracle")
+        for inp in subject.default_test_inputs:
+            print(oracle(inp))
+
+
+if __name__ == "__main__":
+    main()
