e6d46de449ba6b2e085824bbb39622ca27cd7068
Martin Eberlein <schuldner-01.vordach@icloud.com>
2023-11-22 22:46:55 +0100

    more tests4py evaluation

--- eva/evaluate_tests4py.py
+++ eva/evaluate_tests4py.py
@@ -3,11 +3,15 @@ import logging
 
 from debugging_benchmark.tests4py_benchmark import (
     PysnooperBenchmarkRepository,
+    MiddleBenchmarkRepository,
+    CookieCutterBenchmarkRepository,
+    FastAPIBenchmarkRepository,
+    BenchmarkRepository
 )
 
 from debugging_framework.tools import Tool
 from debugging_framework.evaluator import Evaluation
-
+from debugging_framework.benchmark import BenchmarkProgram
 from debugging_framework.tools import InputsFromHellEvaluationFuzzer
 
 from tools import EvoGFuzzEvaluationTool
@@ -18,18 +22,27 @@ def main():
 
     tools: List[Type[Tool]] = [
         InputsFromHellEvaluationFuzzer,
-        # EvoGFuzzEvaluationTool
+        EvoGFuzzEvaluationTool
     ]
 
-    subjects = PysnooperBenchmarkRepository().build()
+    repositories: List[BenchmarkRepository] = [
+        PysnooperBenchmarkRepository(),
+        CookieCutterBenchmarkRepository(),
+        FastAPIBenchmarkRepository(),
+    ]
+
+    subjects: List[BenchmarkProgram] = []
+    for repository in repositories:
+        subjects_ = repository.build()
+        subjects += subjects_
 
     report = Evaluation(
         tools=tools,
         subjects=subjects,
-        repetitions=1,
+        repetitions=10,
         timeout=3600,
-        out_file="./results.pkl",
-        tool_param={"max_non_terminals": 8, "max_generated_inputs": 100},
+        out_file="./results_tests4py.pkl",
+        tool_param={"max_non_terminals": 10, "max_generated_inputs": 1000, "max_iterations": 10},
     ).run()
 
     print(report)
